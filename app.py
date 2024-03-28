b585d49ba2f942a46738908b03342606976db30f
Thatom28 <thatomatlala28@gmail.com>
2024-03-28 09:38:25 +0200

    day 25

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from pprint import pprint
 from about_bp import about_bp
 import uuid
 
+
 load_dotenv()  # load -> os env (enviroment variables)
-print(os.environ.get("AZURE_DATABASE_URL"))
+print(os.environ.get("AZURE_DATABASE_URL"), os.environ.get("FORM_SECRET_KEY"))
 
 app = Flask(__name__)
 
@@ -20,6 +21,8 @@ app = Flask(__name__)
 connection_string = os.environ.get("AZURE_DATABASE_URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = connection_string
 db = SQLAlchemy()
+# Token
+app.config["SECRET_KEY"] = os.environ.get("FORM_SECRET_KEY")
 
 
 # model (SQLAlchemy)  == schema
@@ -49,6 +52,27 @@ class Movie(db.Model):
         }
 
 
+# ----------------------------------------------------------------------------------------------
+# Home work
+
+# class User(db.Model):
+#     # the table name to point to
+#     __tablename__ = "users"
+#     # add its columns                  #it will create random string for id| no need to add
+#     id = db.Column(db.String(50), primary_key=True, default=lambda: str(uuid.uuid4()))
+#     username = db.Column(db.String(50))
+#     password = db.Column(db.String(50))
+
+
+#     # how the data should loook like in JSON (the keys)
+#     def user_to_dict(self):
+#         # the name the front end wants the key to be
+#         return {
+#             "id": self.id,
+#             "username": self.username,
+#             "password": self.password,
+#         }
+# --------------------------------------------------------------------------------------------
 class User(db.Model):
     # the table name to point to
     __tablename__ = "users"
@@ -66,51 +90,16 @@ class User(db.Model):
             "password": self.password,
         }
 
-    db.init_app(app)
 
-    try:
-        with app.app_context():
-            # Use text() to explicitly declare your SQL command
-            result = db.session.execute(text("SELECT 1")).fetchall()
-            print("Connection successful:", result)
-    except Exception as e:
-        print("Error connecting to the database:", e)
+db.init_app(app)
 
-
-# Task 4 | db.session.delete(movie)
-# @app.delete("/movies/<id>")
-# def delete_movie(id):
-#     # Permission to modify the lexical scope variable
-#     filtered_movie = Movie.query.get(id)
-#     if not filtered_movie:
-#         return jsonify({"message": "Movie not found"}), 404
-
-#     try:
-#         data = filtered_movie.to_dict()
-#         db.session.delete(filtered_movie)
-#         db.session.commit()  # Making the change (update/delete/create) permanent
-#         return jsonify({"message": "Deleted Successfully", "data": data})
-#     except Exception as e:
-#         db.session.rollback()  # Undo the change
-#         return jsonify({"message": str(e)}), 500
-
-# --------------------------------------------------------------------------------------------------------------------
-# Task 5: delete on the wweb page
-# @app.route("/movies/delete/<id>")  # <> converts to a variable
-# def delete_movie(id):
-#     movie = Movie.query.get(id)
-#     try:
-#         data = movie.to_dict()
-#         db.session.delete(movie)
-#         # commit for (create|delete|update)
-#         db.session.commit()  # deleted permanently, cannot rollback
-#         return render_template("movie_list")
-#     except Exception as e:
-#         db.session.rollback()  # undo the change
-#         return jsonify({"message": "movie not found"})
-
-
-# --------------------------------------------------------------------------------------------
+try:
+    with app.app_context():
+        # Use text() to explicitly declare your SQL command
+        result = db.session.execute(text("SELECT 1")).fetchall()
+        print("Connection successful:", result)
+except Exception as e:
+    print("Error connecting to the database:", e)
 
 
 # the home page what should we return
@@ -137,7 +126,7 @@ def welcome_page():
     return render_template("welcome_page.html", username=username, password=password)
 
 
-# --------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 @app.route("/movie/add")
@@ -146,32 +135,6 @@ def add_movie():
 
 
 # ----------------------------------------------------------------------------
-
-
-# @app.route("/movie/update/<id>")
-# def edit_movie_by_id(id):
-#     movie = Movie.query.get(id)
-#     if movie:
-#         movie_name = request.form.get("movie_name", movie.name)
-#         movie_poster = request.form.get("movie_poster", movie.poster)
-#         movie_rating = request.form.get("movie_rating", movie.rating)
-#         movie_summary = request.form.get("movie_summary", movie.summary)
-#         movie_trailer = request.form.get("movie_trailer", movie.trailer)
-#     movie = Movie(
-#         name=movie_name,
-#         poster=movie_poster,
-#         rating=movie_rating,
-#         summary=movie_summary,
-#         trailer=movie_trailer,
-#     )
-#     try:
-#         db.session.commit()
-#         return jsonify({"message": "Updated Successfully", "data": movie.to_dict()})
-#     except Exception as e:
-#         return jsonify({"message": "Movie not found"}), 404
-
-
-# -----------------------------------------------------------------------------
 
 
 app.register_blueprint(about_bp, url_prefix="/about")
@@ -194,6 +157,40 @@ app.register_blueprint(movie_list_bp, url_prefix="/movie_list")
 from user_bp import user_bp
 
 app.register_blueprint(user_bp, url_prefix="/user")
+
+# -------------------------------------------------------------------------------
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+
+
+class RegistrationForm(FlaskForm):
+    # the fields (How they look on the template, the validators to the form)
+    username = StringField("Username", validators=[InputRequired(), Length(min=6)])
+    password = PasswordField(
+        "Password", validators=[InputRequired(), Length(min=8, max=12)]
+    )
+
+    submit = SubmitField("sign up")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register_page():
+    form = RegistrationForm()
+    # (on "POST" if the submit button is clicked)
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        new_user = User(username=username, password=password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return "<h1>Registration successful"
+        except Exception as e:
+            db.session.rollback()
+            return f"<h1>Error happend {str(e)}</h1>", 500
+    # for GET
+    return render_template("register.html", form=form)
 
 
 if __name__ == "__main__":
