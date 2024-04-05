3526b7c4c71c5d4a87c42b9ef8bc5015af309fe2
Thatom28 <thatomatlala28@gmail.com>
2024-04-05 14:12:53 +0200

    using Azure database

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ from models.users import User
 # username = 'MD/E1005292'
 # driver_name = "ODBC Driver 17 for SQL Server"
 # connection_string = f"mssql+pyodbc://{username}:@{server}/{database}?driver={driver_name}"
-
 load_dotenv()  # load -> os env (enviroment variables)
 print(os.environ.get("AZURE_DATABASE_URL"), os.environ.get("FORM_SECRET_KEY"))
 
